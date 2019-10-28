9f19df72598b871c388712d656655d5ae6a9dec3
krystek77 <krystian.wrona77@gmail.com>
2019-10-28 13:32:48 +0100

    Add page for TX9 dryers and styles

---
+++ incls/suszarki/TX9_HP.php
@@ -0,0 +1,327 @@
+<?php
+header("Content-Type: text/html; charset=utf-8");
+?>
+<!-- article class="productinfo" -->
+<div class="seria-tx">
+    <header class="header">
+        <h2 class="header--title">Suszarka bębnowa, model TX9</h2>
+        <span>podgrzew elektryczny</span>
+        <span> | </span>
+        <span>podgrzew za pomocą pompy ciepła</span>
+        <p class="desc">
+            Nowa gama suszarek bębnowych o załadunku 9 kg, seria TX, zastępuje poprzednią generację suszarek bębnowych T 9.
+            Wyposażona została w bęben z technologią <strong>DimpleDry<sup>TM</sup></strong> - specjalne wypłukości na powierchni bębna węwnętrznego oraz większa głębokość bębna powodują, że ciepłe powietrze przechodzi przez całą długość bębna a tym samym suszone tkaniny. Dzięki temu rozwiązaniu udało się znacznie skrócić czas suszenia,
+            podnieść wydajność, dodatkowo chroniąc jeszcze lepiej tkaniny.
+        </p>
+    </header>
+    <div>
+
+        <div class="wrapper introduction">
+            <div>
+                <h3>Wprowadzenie do nowej serii suszarek TX</h3>
+                <ul>
+                    <li>atrakcyjny, skandynawski design</li>
+                    <li>obudowa lakierowana w kolorze stali INOX</li>
+                    <li>zaprojektowana aby doskonale pasować do pralnicowirówki FX80</li>
+                    <li>w opcji nierdzewne drzwi załadunkowe i panel przedni</li>
+                </ul>
+            </div>
+            <div class="">
+                <img class="image-wrapper__image" src="media/images/suszarki/TX9_EL_HP.png" alt="nowa generacja suszarek seria TX o załadunku 9 kg">
+            </div>
+        </div>
+
+        <div class="wrapper unique">
+            <h3>Co czyni suszarkę TX9 unikalną</h3>
+            <h4>DNA PRIMUS</h4>
+            <p class="desc">
+                Sugestie Klientów doprowadziły nas do stworzenia
+                zupełnie nowej suszarki bębnowej, inspirowanej naszym
+                najlepiej sprzedającym się produktem tj. pralnicowirówką
+                wolnostojącą wysokoobrotową serii FX. Estetyczny wygląd i pewne jej technologie, zostały poraz pierwszy zastosowane
+                w suszarkach.
+            </p>
+            <p class="desc">
+                <strong>Seria TX</strong> łączy w sobie wygląd pasujący do pralnicowirówki FX80, wymiary i parametry dopasowane do
+                pralnicowirówki FX80 oraz nowe technologie, co czyni ten zestaw świetnie współpracującym duetem przemysłowych urządzeń pralniczych.
+            </p>
+            <div class="wrapper-images-row">
+                <div class="images-row">
+                    <div>
+                        <img src="media/images/suszarki/unique_TX_1.png" alt="">
+                    </div>
+                    <div>
+                        <img src="media/images/suszarki/unique_TX_2.png" alt="">
+                    </div>
+                </div>
+                <div class="images-row">
+                    <div>
+                        <img src="media/images/suszarki/unique_TX_3.png" alt="">
+                    </div>
+                    <div>
+                        <img src="media/images/suszarki/unique_TX_4.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <h4>ATRAKCYJNY DUET</h4>
+            <p class="desc">
+                Nowa suszarka <b>TX9</b> doskonale pasuje do pralnicowirówki FX80, pod względem zarówno wyglądu jak i
+                wymiarów. Ten atrakcyjny duet jest idealnym rozwiązaniem dla małych i średnich rozmiarów pralni przemysłowych.
+            </p>
+            <div class="duet">
+                <img src="media/images/suszarki/TXFX_DUO.png" alt="zestaw pralnicowirówka i dopasowana suszarka o załadunku 9 kg">
+            </div>
+        </div>
+
+        <div class="wrapper compare">
+            <h3>Ogólna specyfikacja i porównanie</h3>
+            <div>
+                <div class="images-row">
+                    <div>
+                        <img src="media/images/suszarki/T9_vs_TX9_EL.png" alt="porównanie wysokości suszarki T 9 i TX 9">
+                    </div>
+                    <div>
+                        <img src="media/images/suszarki/T9_HP_vs_TX9_HP.png" alt="porównanie wysokości suszarki T9 HP i TX9 HP">
+                    </div>
+                </div>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>T9 EL</th>
+                                <th>TX9 EL</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="left">wys. x. sz. x gł. (mm)</td>
+                                <td>1465x795x815</td>
+                                <td>1150x710x945</td>
+                            </tr>
+                            <tr class="even-row">
+                                <td class="left">masa netto (kg)</td>
+                                <td>170</td>
+                                <td>125</td>
+                            </tr>
+                            <tr>
+                                <td class="left">powierzchnia podstawy (m <sup>2</sup>)</td>
+                                <td>0,648</td>
+                                <td>0.671</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table>
+                        <thead>
+                            <th></th>
+                            <th>T9 HP</th>
+                            <th>TX9 HP</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="left">wys. x. sz. x gł. (mm)</td>
+                                <td>1465x795x1275</td>
+                                <td>1725x710x925</td>
+                            </tr>
+                            <tr class="even-row">
+                                <td class="left">masa netto (kg)</td>
+                                <td>290</td>
+                                <td>193</td>
+                            </tr>
+                            <tr>
+                                <td class="left">powierzchnia podstawy (m <sup>2</sup>)</td>
+                                <td>1.014</td>
+                                <td>0.657</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="wrapper control">
+            <div class="first-row">
+                <div>
+                    <img src="media/images/suszarki/TX_EC.png" alt="sterownik suszarek serii TX">
+                </div>
+                <div>
+                    <h3>Sterowanie</h3>
+                    <h4>Sterownik mikroprocesorowy <strong>Easy Control</strong></h4>
+                    <ul>
+                        <li>wersja samoobsługowa lub tradycyjna</li>
+                        <li>3 programy suszenia</li>
+                        <li>swobodne programowanie temperatury suszenia</li>
+                        <li>intuicyjna obsługa za pomocą prostych wyborów</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="second-row">
+                <img src="media/images/suszarki/TX_EC_panel.png" alt="panel kontrolny suszarek serii TX">
+            </div>
+        </div>
+
+        <div class="wrapper drum">
+            <h3>Bęben wewnętrzny</h3>
+            <p class="desc">
+                Bęben wewnętrzny suszarki TX9 oferuje wyższą wydajność i krótszy cykl suszenia, dzięki innowacyjnym połączeniu głębokości bębna ze specjalnym kształetem powłoki bębna.
+            </p>
+            <div class="images-row">
+                <div>
+                    <img src="media/images/suszarki/TX_dimpleDrum_1.png" alt="kształ bębna wewnętrznego suszarki seri TX">
+                </div>
+                <div>
+                    <img src="media/images/suszarki/TX_dimpleDrum_2.png" alt="kształt bębna wewnętrznego suszarki seri TX">
+                </div>
+            </div>
+            <table>
+                <thead>
+                    <th></th>
+                    <th>Podgrzew elektryczny</th>
+                    <th>Pompa ciepła</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Pojemność bębna</td>
+                        <td colspan="2">195 litrów</td>
+                    </tr>
+                    <tr>
+                        <td>Średnica bębna</td>
+                        <td colspan="2">670 mm</td>
+                    </tr>
+                    <tr>
+                        <td>Głębokość bębna</td>
+                        <td colspan="2">555 mm</td>
+                    </tr>
+                    <tr>
+                        <td>Przepływ powietrza</td>
+                        <td colspan="2">osiowy</td>
+                    </tr>
+                    <tr>
+                        <td>Kształt bębna</td>
+                        <td colspan="2"><strong>DimpleDry<sup>TM</sup></strong></td>
+                    </tr>
+                    <tr>
+                        <td>Liczba przewałów</td>
+                        <td colspan="2">3</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div>
+                <div>
+                    <img src="" alt="">
+                </div>
+                <p class="desc">
+                    Seria suszarek TX ma o 32% głębszy bęben dla dłuższego i bardziej wydajnego przepływu powietrza niż w suszarkach T9 -
+                    poprzedniej generacji.
+                </p>
+            </div>
+        </div>
+
+        <div class="wrapper spec">
+            <h3>Podstawowe dane techniczne</h3>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>TYP</th>
+                        <th>TX9 EL</th>
+                        <th>TX9 HP</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="left">Załadunek (kg)</td>
+                        <td colspan="2">9</td>
+                    </tr>
+                    <tr class="even-row">
+                        <td class="left">Pojemność bębna (litry)</td>
+                        <td colspan="2">195</td>
+                    </tr>
+                    <tr>
+                        <td class="left">Średnica bębna (mm)</td>
+                        <td colspan="2">670</td>
+                    </tr>
+                    <tr class="even-row">
+                        <td class="left">Głębokość bębna (mm)</td>
+                        <td colspan="2">555</td>
+                    </tr>
+                    <tr>
+                        <td class="left">Średnica okna (mm)</td>
+                        <td colspan="2">500</td>
+                    </tr>
+                    <tr class="even-row">
+                        <td class="left">Kierunek otwarcia</td>
+                        <td colspan="2">prawy (w opcji: lewy)</td>
+                    </tr>
+                    <tr>
+                        <td class="left">Przepływ powietrza</td>
+                        <td colspan="2">osiowy</td>
+                    </tr>
+                    <tr class="even-row">
+                        <td class="left">Przepływ (m3/godz.)</td>
+                        <td>300</td>
+                        <td>-</td>
+                    </tr>
+                    <tr>
+                        <td class="left">Podgrzew</td>
+                        <td>elektryczny</td>
+                        <td>pompa ciepła</td>
+                    </tr>
+                    <tr class="even-row">
+                        <td class="left">Moc podgrzewu (kW)</td>
+                        <td>6 /9 / 12</td>
+                        <td>1.5</td>
+                    </tr>
+                    <tr>
+                        <td class="left">Moc zainstalowana (kW)</td>
+                        <td>6.4 / 9.4 / 12.4 </td>
+                        <td>2</td>
+                    </tr>
+                    <tr class="even-row">
+                        <td class="left">Zasilanie</td>
+                        <td colspan="2">3x380-415V+N 50Hz</td>
+                    </tr>
+                    <tr>
+                        <td class="left">Moc silnika (kW)</td>
+                        <td colspan="2">0.25 </td>
+                    </tr>
+                    <tr class="even-row">
+                        <td class="left">Moc wentylatora (kW)</td>
+                        <td>0.25</td>
+                        <td>0.17</td>
+                    </tr>
+                    <tr>
+                        <td class="left">Wylot powietrza (mm)</td>
+                        <td>150</td>
+                        <td> brak </td>
+                    </tr>
+                    <tr class="even-row">
+                        <td class="left">Podłączenie wody (cal)</td>
+                        <td>brak</td>
+                        <td>2x1</td>
+                    </tr>
+                    <tr>
+                        <td class="left">Odprowadzenie kondensatu (mm)</td>
+                        <td>brak</td>
+                        <td>16</td>
+                    </tr>
+                    <tr class="even-row">
+                        <td class="left">Hałas (dB)</td>
+                        <td>55</td>
+                        <td>54</td>
+                    </tr>
+                    <tr>
+                        <td class="left">Wymiary (mm)</td>
+                        <td>1150 x 710 x 945</td>
+                        <td>1725 x 710 x 925</td>
+                    </tr>
+                    <tr class="even-row">
+                        <td class="left">HWaga netto (kg)</td>
+                        <td>125</td>
+                        <td>193</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
